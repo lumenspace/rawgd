078f702e4c0f5eb8b344db0154c8f1c09a957ccf
najimovs <lumenkowski@gmail.com>
2025-02-15 07:42:41 +0500

    Test RAWGD geometry in a WebGL (THREE.JS) scene

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
+import * as THREE from "three"
 import * as RAWGD from "@lib/rawgd"
+import { setupScene } from "./setupScene"
+
+const { scene } = setupScene( document.getElementById( "gl" ) )
 
 // Test a binary file
 {
-	const buffer = await ( await fetch( "/sample-geometries/plane.rawgd" ) ).arrayBuffer()
+	const buffer = await ( await fetch( "/sample-geometries/sphere.rawgd" ) ).arrayBuffer()
 
-	console.log( RAWGD.decode( buffer ) )
+	const { indices, vertices, normals } = RAWGD.decode( buffer )
+
+	const geometry = new THREE.BufferGeometry()
+
+	if ( indices !== null ) {
+
+		geometry.setIndex( new THREE.BufferAttribute( indices, 1 ) )
+	}
+
+	geometry.setAttribute( "position", new THREE.BufferAttribute( vertices, 3 ) )
+
+	if ( normals !== null ) {
+
+		geometry.setAttribute( "normal", new THREE.BufferAttribute( normals, 3 ) )
+	}
+
+	//
+
+	scene.add( new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { flatShading: true } ) ) )
 }
+
+// const geometry = new THREE.SphereGeometry( 5 )
+// scene.add( new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { flatShading: true } ) ) )
+
+// const buffer = RAWGD.encode( {
+// 	vertices: geometry.attributes.position.array,
+// 	indices: geometry.index.array,
+// 	normals: geometry.attributes.normal.array,
+// } )
 
 // const vertices = new Float32Array( [ - 5, 0, 5, 5, 0, 5, 5, 0, - 5, - 5, 0, - 5 ] )
 // const indices = new Uint16Array( [ 0, 1, 2, 2, 3, 0 ] )
@@ -13,7 +44,7 @@ import * as RAWGD from "@lib/rawgd"
 
 // const buffer = RAWGD.encode( { vertices, indices, normals } )
 
-// downloadGeometry( buffer, "geometry.rawgd" )
+// downloadGeometry( buffer, "sphere.rawgd" )
 
 // Decode
 // {
---
+++ src/setupScene.js
@@ -0,0 +1,52 @@
+import * as THREE from "three"
+import { MapControls } from "three/addons/controls/MapControls"
+
+export function setupScene( canvas ) {
+
+	const scene = new THREE.Scene()
+
+	const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100 )
+	camera.position.set( 10, 15, 10 )
+	camera.lookAt( 0, 0, 0 )
+
+	const controls = new MapControls( camera, canvas )
+
+	const renderer = new THREE.WebGLRenderer( { canvas } )
+	renderer.setPixelRatio( window.devicePixelRatio )
+	renderer.setSize( window.innerWidth, window.innerHeight )
+
+	const render = () => {
+
+		requestAnimationFrame( render )
+
+		renderer.render( scene, camera )
+	}
+
+	render()
+
+	//
+
+	{
+		const dirLight = new THREE.DirectionalLight( 0xffffff, 0.5 )
+		dirLight.position.set( - 2, 20, 4 )
+		scene.add( dirLight )
+	}
+
+	{
+		const dirLight = new THREE.DirectionalLight( 0xffffff, 0.5 )
+		dirLight.position.set( 2, 20, - 4 )
+		scene.add( dirLight )
+	}
+
+	const hemiLight = new THREE.HemisphereLight( 0xcaf0f8, 0xd5bdaf )
+	hemiLight.position.set( 0, 20, 0 )
+	scene.add( hemiLight )
+
+	//
+
+	return {
+		scene,
+		camera,
+		renderer,
+	}
+}
